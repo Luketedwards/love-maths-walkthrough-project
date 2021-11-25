78c34656878873a075773cd0090318dd5d85fca4
Luketedwards <Luketedwards96@hotmail.com>
2021-11-25 13:59:32 +0000

    Add event listeners for buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons){
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type")=== "submit"){
+                alert("You clicked Submit!");
+            } else{
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    } 
+})
+
 function runGame(){
 
 }
@@ -27,5 +42,5 @@ function displaySubtractQuestion(){
 }
 
 function displayMultiplyQuestion(){
-    
+
 }

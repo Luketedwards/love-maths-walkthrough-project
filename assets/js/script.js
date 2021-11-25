aa4ad673cae3f646afb651cc9c7ed9dc7257a63c
Luketedwards <Luketedwards96@hotmail.com>
2021-11-25 14:34:16 +0000

    Add code to check if answer was correct

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function(){
     for (let button of buttons){
         button.addEventListener("click", function(){
             if (this.getAttribute("data-type")=== "submit"){
-                alert("You clicked Submit!");
+                checkAnswer();
             } else{
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -29,11 +29,30 @@ function runGame(gameType){
 }
 
 function checkAnswer(){
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
 
+    if (isCorrect){
+        alert('You got it right!');
+    } else{
+        alert(`Aw you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+    }
+
+    runGame(calculatedAnswer[1]);
 }
 
 function calculateCorrectAnswer(){
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById("operator").innerText;
 
+    if (operator === "+"){
+        return [operand1 + operand2, "addition"];
+    } else 
+    { alert(`Unimplemented operator ${operator}`);
+      throw `Unimplemented operator ${operator}. Aborting!`;
+}
 }
 
 function incrementScore(){

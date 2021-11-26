251603e8c325ac975fb472c76a353caf15df8389
Luketedwards <Luketedwards96@hotmail.com>
2021-11-26 15:02:53 +0000

    Add division code

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,7 +36,10 @@ function runGame(gameType){
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract"){
         displaySubtractQuestion(num1, num2);
-    } else {
+    } else if (gameType === "division"){
+        displayDivisionQuestion(num1, num2);
+    }
+    else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -70,6 +73,9 @@ function calculateCorrectAnswer(){
     }
     else if (operator === "-"){
         return [operand1 - operand2, "subtract"];
+    } else if (operator === "/"){
+        
+        return [operand1 / operand2, "division"]
     }
     else 
     { alert(`Unimplemented operator ${operator}`);
@@ -106,4 +112,10 @@ function displayMultiplyQuestion(operand1, operand2){
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+}
+
+function displayDivisionQuestion (operand1, operand2){
+    document.getElementById('operand1').textContent = (operand1 * operand2);
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "/";
 }

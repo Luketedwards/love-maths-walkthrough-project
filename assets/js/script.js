2d4c93e800fe7f9aa4c625690b1df055537ad512
Luketedwards <Luketedwards96@hotmail.com>
2021-11-26 14:35:57 +0000

    Add enter key control for answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,11 +12,21 @@ document.addEventListener("DOMContentLoaded", function(){
         })
     } 
 
+    document.getElementById('answer-box').addEventListener('keydown', function(event) {
+        if (event.key === "Enter"){
+            checkAnswer();
+        }
+    })
+
     runGame("addition");
 })
 
 /** creates 2 random numbers between 1 and 25 */
 function runGame(gameType){
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     let num1 = Math.floor(Math.random() *25) +1;
     let num2 = Math.floor(Math.random() *25) +1;
 

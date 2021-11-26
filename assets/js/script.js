9857202f4349ed816d61b99a20ab31a5fbeeaab3
Luketedwards <Luketedwards96@hotmail.com>
2021-11-26 14:10:02 +0000

    Add mulitplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,7 +22,9 @@ function runGame(gameType){
 
     if(gameType === "addition"){
         displayAdditionQuestion(num1, num2);
-    } else {
+    } else if (gameType === "multiply"){
+        displayMultiplyQuestion(num1, num2);
+    }else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -51,7 +53,10 @@ function calculateCorrectAnswer(){
 
     if (operator === "+"){
         return [operand1 + operand2, "addition"];
-    } else 
+    } else if (operator === "x"){
+        return [operand1 * operand2, "multiply"];
+    }
+    else 
     { alert(`Unimplemented operator ${operator}`);
       throw `Unimplemented operator ${operator}. Aborting!`;
 }
@@ -80,6 +85,8 @@ function displaySubtractQuestion(){
 
 }
 
-function displayMultiplyQuestion(){
-
+function displayMultiplyQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 }
